372ee6f5dcdc387a8b13449fb9e676c53638a793
Dragiinus <melvinmaute@gmail.com>
2023-02-01 14:40:23 +0100

    Router done

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In doloribus accusantium commodi, eum aliquid odio unde repudiandae ab voluptatum odit quo ratione dolor, nemo maxime corporis ipsam perspiciatis, inventore suscipit.</p>
+        </div>
+    );
+};
+
+export default About;
