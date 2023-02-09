45b0b3ef62ab5351186e3dfd3706e799cd903d95
Dragiinus <melvinmaute@gmail.com>
2023-02-09 20:55:21 +0100

    Final version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/Projet-7-Openclassroom/public/index.html">
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
